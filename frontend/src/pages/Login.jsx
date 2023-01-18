46a2d7747d26f82590e15bf02a04ec4050b1984a
gouseimmu <gouseimmugh@gmail.com>
2023-01-18 20:46:22 +0530

    Signin And signup Ui done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import styles from "./login.module.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div id={styles.Container}>
+      <div id={styles.Box1}>
+        <div id={styles.sub1}>
+          <div id={styles.name}>
+            <h1>Adorama</h1>
+          </div>
+          <div id={styles.new}>
+            <h2>New Customer ?</h2>
+            <p>Create New Account {">"}</p>
+          </div>
+        </div>
+        <div id={styles.sub2}>
+          <div id={styles.sign}>
+            <h1>Sign In</h1>
+            <div>
+              <form>
+                <label>Your Email :</label>
+                <br />
+                <input type="text" placeholder="enter mail" required />
+                <br />
+                <label>Your Password :</label>
+                <br />
+                <input type="text" placeholder="enter Password" required />
+                <label>
+                  <input type="checkbox" name="remember" /> Remember me
+                </label>
+                <input type="submit" value="LOGIN" />
+                <br />
+              </form>
+              <p id={styles.ps}>Forgot Password ?</p>
+            </div>
+            <div>
+            <hr  styles={{"width":"15px"}}/>
+            </div>
+            <div id={styles.good}>
+              <img src="https://blog.hubspot.com/hubfs/image8-2.jpg" alt="df" />
+              <button>   Sign in With Google</button>
+              <div id={styles.apple}>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1667px-Apple_logo_black.svg.png" alt="apple" />
+              </div>
+              <button>Sign in With Apple</button>
 
-export default Login
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+ 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import styles from "./signup.module.css";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div id={styles.Container}>
+      <div id={styles.Box1}>
+        <div id={styles.sub1}>
+          <div id={styles.name}>
+            <h1>Adorama</h1>
+          </div>
+          <div id={styles.new}>
+            <h2>New Customer ?</h2>
+            <p>Create New Account {">"}</p>
+          </div>
+        </div>
+        <div id={styles.sub2}>
+          <div id={styles.sign}>
+            <h1>Create An Account</h1>
+            <div>
+              <form>
+                <div id={styles.fl}>
+             
+                 
+                 <input type="text" placeholder="First Name" required /><br />
+                  
+                 <input type="text" placeholder="Last Name" required /><br />
+                </div>
+             
+              
 
-export default Signup
+                 
+                
+                <label id={styles.ps}>Your Email :</label>
+                <br />
+                <input id={styles.ps} type="text" placeholder="enter mail" required />
+                <br />
+                <label>Your Password :</label>
+                <br />
+                <input type="text" placeholder="enter Password" required />
+                
+                <label>Confirm Password :</label>
+                <br />
+                <input type="text" placeholder="Confirm Password" required />
+               
+                <div id={styles.lab}>
+                <label>
+                  <input type="checkbox" name="remember" /> Yes, I would like to receive expert tips, special offers, and product news.
+                </label><br />
+                <label>
+                  <input type="checkbox" name="remember" />  Yes, sign me up for Adorama Rewards.
+                </label><br />
+                <label>
+                  <input type="checkbox" name="remember" />  Remember Me.
+                </label><br />
+                </div>
+                
+                <input type="submit" value="CREATE ACCOUNT" />
+                 
+              </form>
+        
+            </div>
+            
+         </div>
+         </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
+
+ 
