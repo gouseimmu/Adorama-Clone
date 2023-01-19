2d0c49c7c365284a54fad733d09677ee0db2b086
Nikhil-81 <nikhilwalwatkar81@gmail.com>
2023-01-19 15:24:31 +0530

    root_basic_css

--- frontend/src/components/Home_component/brands/brand_ceros.js
+++ frontend/src/components/Home_component/brands/brand_ceros.js
@@ -11,7 +11,43 @@ export default function Brand_cerosel({brand_data}) {
         slidesToShow: 7,
         slidesToScroll: 3,
         cssEase: "linear",
-      
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+                breakpoint: 734,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 565,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 435,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },]
     };
     return (
 <div className="category_cerosel" Style={"margin: auto;display:block;width: 95.5%;padding:2rem 0;"} >
