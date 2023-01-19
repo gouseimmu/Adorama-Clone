72d72f097db4dbbb49e5a37647f277a42a7eb13b
rajkumarsahu89 <sahukumarraj89@gmail.com>
2023-01-19 23:05:13 +0530

    day-3

--- frontend/src/components/product/Product.jsx
+++ frontend/src/components/product/Product.jsx
@@ -1,81 +1,135 @@
 import React from 'react'
-import  "../product/product.css"
+import "../product/product.css"
 
 
 const product = () => {
-  return (
-    <>
-    <div className='pro1'>
+    return (
+        <>
+            <div className='pro1'>
 
-       <div className='pro2'>
-       <h1>Photography</h1>
-       </div>
-        
-    </div>
-    <div className='pro3'>
-        <h2>Photography by Category</h2>
-        <a>Shop Used Photography (12113)</a>
-    </div>
+                <div className='pro2'>
+                    <h1>Photography</h1>
+                </div>
 
-    <div className='pro4'>
-        <div>
-            <img src="https://www.adorama.com/images/Large/ipczs80s.jpg" alt="sony" />
-            <span>Cameras</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/cms/15616lenses_47765.jpg" alt="sony" />
-            <span>Lenses</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/Large/we4700.jpg" alt="sony" />
-            <span>Lighting & Studio</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/Large/3pactth_1.jpg" alt="sony" />
-            <span>Tripods & Supports</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/Large/icabgr10_1.jpg" alt="sony" />
-            <span>Camera Accessories</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/Large/iommc20.jpg" alt="sony" />
-            <span>Lens Accessories</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/Large/kkp40036pp.jpg" alt="sony" />
-            <span>Film & Darkroom Equipment</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/cms/15616Lens_Filter_37689.jpg" alt="sony" />
-            <span>Lens Filters</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/cms/15616underwater_50466.jpg" alt="sony" />
-            <span>Underwater Photography</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/cms/15616mobile_photography_89004.jpg" alt="sony" />
-            <span>Mobile Photography</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/cms/15616frames_53390.jpg" alt="sony" />
-            <span>Photo Albums, Frames & Storage</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/cms/15616backpacks_10788.jpg" alt="sony" />
-            <span>Photography Bags & Cases</span>
-        </div>
-        <div>
-            <img src="https://www.adorama.com/images/cms/15616memory_card_photography_78143.jpg" alt="sony" />
-            <span>Memory Cards</span>
-        </div>
+            </div>
+            <br />
+            <div className='pro3'>
+                <h2>Photography by Category</h2>
+                <a>Shop Used Photography (12113)</a>
+            </div>
 
-    </div>
-    </>
-  
-    
-  )
+            <div className='pro4'>
+                <div className='pro5'>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/Large/ipczs80s.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                            <span>Cameras</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/cms/15616lenses_47765.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Lenses</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/Large/we4700.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Lighting & Studio</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/Large/3pactth_1.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Tripods & Supports</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/Large/icabgr10_1.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Camera Accessories</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/Large/iommc20.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Lens Accessories</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/Large/kkp40036pp.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Film & Darkroom Equipment</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/cms/15616Lens_Filter_37689.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Lens Filters</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/cms/15616underwater_50466.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Underwater Photography</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/cms/15616mobile_photography_89004.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Mobile Photography</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/cms/15616frames_53390.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Photo Albums, Frames & Storage</span>
+                        </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/cms/15616backpacks_10788.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Photography Bags & Cases</span>
+                    </div>
+                    </div>
+                    <div className='pro6' >
+                        <div className='circle'>
+                            <img src="https://www.adorama.com/images/cms/15616memory_card_photography_78143.jpg" alt="sony" />
+                        </div>
+                        <div className='spa'>
+                        <span>Memory Cards</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+
+    )
 }
 
 export default product
