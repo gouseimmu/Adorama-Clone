10061614955f2c306bc44369f873814c0b87b3c0
mdalimujtaba <mdalimujtaba29850@gmail.com>
2023-01-20 19:12:17 +0530

    cart update delete

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -19,9 +19,6 @@ cartRoute.get("/", async (req, res) => {
 cartRoute.post("/additem",async(req,res)=>{
     // console.log('item added')
     const {productID,quantity,userID}=req.body
-  
-    
-    
         try {
                 let data = await CartModel.create({ productID, quantity, userID });
                 console.log(data)
@@ -34,6 +31,53 @@ cartRoute.post("/additem",async(req,res)=>{
           }
     
 })
+cartRoute.patch("/update", async (req, res) => {
+  let { type, productID,userID } = req.body;
+
+  let existingcartItems = await CartModel.findOne({ userID, productID });
+  try {
+    if (type === "dec") {
+      let updatedData = await CartModel.findByIdAndUpdate(
+        existingcartItems._id,
+        { $set: { quantity: existingcartItems.quantity - 1 } }
+      );
+      // console.log(existingcartItems);
+      res.send({
+        message: "item qty decremented",
+      });
+    } else {
+      let updatedData = await CartModel.findByIdAndUpdate(
+        existingcartItems._id,
+        { $set: { quantity: existingcartItems.quantity + 1 } }
+      );
+
+      return res.send({
+        message: "item qty incremented",
+      });
+    }
+  } catch (e) {
+    return res.status(500).send("something went wrong");
+  }
+});
+
+cartRoute.delete("/delete", async (req, res) => {
+  let { productID,userID } = req.body;
+  // let userId = req.headers.userid;
+  // console.log(type,productId,userId)
+
+  let existingcartItems = await CartModel.findOne({ userID, productID });
+  // console.log(existingcartItems);
+  if (existingcartItems) {
+    try {
+      await CartModel.findByIdAndDelete(existingcartItems._id);
+      return res.send("item deleted from cart");
+    } catch {
+      return res.status(500).send(e.message);
+    }
+  } else {
+    return res.status(500).send("something went wrong");
+  }
+});
 
 module.exports={
     cartRoute
