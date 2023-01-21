0936cea7c39427a5fb71953e12493ed27acaa067
Nikhil-81 <nikhilwalwatkar81@gmail.com>
2023-01-22 01:52:01 +0530

    cart_css_updated

--- frontend/src/components/cart_components/cart_card/cart_card.js
+++ frontend/src/components/cart_components/cart_card/cart_card.js
@@ -9,7 +9,7 @@ export default function Cart_card({item_image,item_discription,item_count,item_c
         <div className="cart_cart_image" ><img  src={item_image} /></div>
         <div className="cart_cart_dic" ><Text>{item_discription}</Text></div>
         <div className="cart_cart_qty" ><Button color="white" background="#003F7F" disabled={item_count==1} onClick={hendle_reduce_button} >-</Button><span Style={"padding: 7px;"} >{item_count}</span><Button color="white" background="#003F7F" onClick={hendle_increase_button} >+</Button></div>
-        <div className="cart_cart_cost" ><Text>{item_cost}</Text></div>
+        <div className="cart_cart_cost" ><Text>{item_cost} &#8377;</Text></div>
         <div className="cart_cart_extras" >
             <Text>Save for later</Text>
             <Text>Accessories</Text>
